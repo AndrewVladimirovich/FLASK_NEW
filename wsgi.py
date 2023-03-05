fe727e397291f6dd53513e588af37a2b26bff214
Tretyakov Andrey <94076226+AndrewVladimirovich@users.noreply.github.com>
2023-03-05 23:17:56 +0300

    Lesson 2 (#2)
    * Lesson_2 create templates, views
    
    * Lesson_2 create templates, views
    
    * Lesson_2 add static
    
    * Lesson_2 add nav
    
    * Lesson_2 articles
    
    * Lesson_2 add articles in app
    
    * Lesson_2 lst chng

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,6 @@
-from blog.app import app
+from blog.app import create_app
 
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=80,
-        debug=True,
-    )
+app = create_app()
+
 
     
