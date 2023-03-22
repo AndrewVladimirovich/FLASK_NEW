3d15b1c13df43a49906075ec1d6c3fe7d50a2b0e
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-03-22 21:24:20 +0400

    Lesson_5 modeficate  user.py

--- blog/models/user.py
+++ blog/models/user.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from blog.models.database import db
 from flask_login import UserMixin
+from sqlalchemy import Column, Integer, String, Boolean, LargeBinary
+from blog.security import flask_bcrypt
 
 
 class User(db.Model, UserMixin):
@@ -10,3 +12,15 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"<User #{self.id} {self.username!r}>"
     email = Column(String(255), nullable=False, default="", server_default="")
+    _password = Column(LargeBinary, nullable=True)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = flask_bcrypt.generate_password_hash(value)
+
+    def validate_password(self, password) -> bool:
+        return flask_bcrypt.check_password_hash(self._password, password)
