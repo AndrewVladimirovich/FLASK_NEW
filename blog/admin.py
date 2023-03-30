0369104f4da051470934723f1aaa31b466ce55e1
Andrey Tretyakov <andrew1_2002@mail.ru>
2023-03-30 22:25:54 +0400

    Lesson_8 denaied access

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,29 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from blog import models
 from blog.models.database import db
+from flask import redirect, url_for
+from flask_login import current_user
+
+class MyAdminIndexView(AdminIndexView):
+    @expose("/")
+    def index(self):
+        if not (current_user.is_authenticated and current_user.is_staff):
+            return redirect(url_for("auth_app.login"))
+        return super(MyAdminIndexView, self).index()
+# Create admin with custom props
+admin = Admin(
+    name="Blog Admin",
+    index_view=MyAdminIndexView(),
+    template_mode="bootstrap4",
+)
 
 # Customized admin interface
 class CustomView(ModelView):
-    pass
-# Create admin with custom base template
-admin = Admin(name="Blog Admin", template_mode="bootstrap4")
-# Add views
-admin.add_view(CustomView(models.Tag, db.session, category="Models"))
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_staff
+def inaccessible_callback(self, name, **kwargs):
+# redirect to login page if user doesn't have access return redirect(url_for("auth_app.login"))
 
 admin.add_view(CustomView(models.Tag, db.session, category="Models"))
 admin.add_view(CustomView(models.Article, db.session, category="Models"))
